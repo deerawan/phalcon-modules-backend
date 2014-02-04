5469bd0304a1ef806e77a2b5b6ef6b6358973a32
Budi Irawan <deerawan@gmail.com>
2014-02-04 14:36:35 +0700

    init phalcon project

---
+++ apps/frontend/controllers/ControllerBase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ezypickup\Frontend\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller
+{
+
+}
---
+++ apps/frontend/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ezypickup\Frontend\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+    	echo 'hello';
+    }
+
+}
+
